35d088ad86b13e9c5d776b72ee1b12e6fcfcf42b
palibrix <palibrix@gmail.com>
2024-05-01 23:49:57 +0300

    Add Receiver. Refactor code, add _Detail models

--- builds/models.py
+++ builds/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 
-class Drone(models.Model):
-    frame = models.ForeignKey('components.Frame', on_delete=models.SET_NULL, null=True)
+# class Drone(models.Model):
+#     frame = models.ForeignKey('components.Frame', on_delete=models.SET_NULL, null=True)
--- components/admin.py
+++ components/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.db.models import F
 from django.utils.translation import gettext_lazy as _
 
+from builds.forms import RequiredInlineFormSet
 from .mixins import IsPublicMixin
-from .models import Frame, Propeller, Camera
+from .models import Frame, Propeller, Camera, FrameDetail, ReceiverDetail, Receiver, AntennaConnector, ReceiverProtocol
+
+admin.site.register(AntennaConnector)
+admin.site.register(ReceiverProtocol)
 
 
 class IsPublicFilter(admin.SimpleListFilter):
@@ -23,12 +29,49 @@ class IsPublicFilter(admin.SimpleListFilter):
         return queryset
 
 
+class FrameDetailInline(admin.StackedInline):
+    model = FrameDetail
+    min_num = 1
+    extra = 0
+    formset = RequiredInlineFormSet
+
+
+class ReceiverDetailInline(admin.StackedInline):
+    model = ReceiverDetail
+    min_num = 1
+    extra = 0
+    formset = RequiredInlineFormSet
+
+
+@admin.register(Receiver)
+class ReceiverAdmin(admin.ModelAdmin, IsPublicMixin):
+    inlines = [ReceiverDetailInline, ]
+    list_display = ('model', 'id', 'processor', 'get_frequency', 'is_public')
+    list_filter = ('manufacturer', IsPublicFilter)
+    search_fields = ('model', 'id', 'processor', 'manufacturer')
+
+    def get_frequency(self, obj):
+        if obj.receiver_details.exists():
+            frequencies = list(obj.receiver_details.values_list('frequency', flat=True))
+            return frequencies
+        return "No ReceiverDetail"
+
+    get_frequency.short_description = _('Frequencies')
+
+
 @admin.register(Frame)
 class FrameAdmin(admin.ModelAdmin, IsPublicMixin):
-
-    list_display = ('model', 'id', 'prop_size', 'size', 'user', 'is_public')
+    inlines = [FrameDetailInline, ]
+    list_display = ('model', 'id', 'prop_size', 'size', 'user', 'is_public', 'frame_detail_info')
     list_filter = ('manufacturer', IsPublicFilter)
     search_fields = ('model', 'id', 'prop_size', 'size')
+
+    def frame_detail_info(self, obj):
+        if obj.frame_details.exists():
+            return obj.frame_details.count()
+        return 'No FrameDetail'
+
+    frame_detail_info.short_description = _('Mounting Types Q-ty')
 
 
 @admin.register(Propeller)
