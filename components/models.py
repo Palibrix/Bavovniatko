11dac2d0dcc4839c30dbe2c12d047151f89a65d2
palibrix <palibrix@gmail.com>
2024-05-01 23:49:57 +0300

    Add Receiver. Refactor code, add _Detail models

--- components/models.py
+++ components/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 from components.mixins import UniqueConstraintMixin
-from components.validators import validate_integer_length
+from components.validators import validate_integer_length, validate_even_number
 
 User = get_user_model()
 
@@ -17,14 +17,47 @@ User = get_user_model()
 #     raise NotImplementedError
 #
 #
-# class Receiver(models.Model):
-#     raise NotImplementedError
-#
-#
+class Receiver(models.Model, UniqueConstraintMixin):
+    fields_public = ['manufacturer', 'model', 'processor', 'voltage_min', 'voltage_max'
+                     ]
+    fields_private = fields_public + ['user', ]
+    error_message = 'A Frame with these attributes already exists for this user.'
+
+    manufacturer = models.CharField(max_length=50)
+    model = models.CharField(max_length=50, help_text="Full name of the item")
+
+    processor = models.CharField(max_length=100, help_text="Name of the processor",
+                                 null=True, blank=True)
+    voltage_min = models.FloatField(validators=[MinValueValidator(2), MaxValueValidator(28)],
+                                    help_text='Voltage Range - Minimal Voltage', verbose_name='Minimal Voltage')
+    voltage_max = models.FloatField(validators=[MinValueValidator(2), MaxValueValidator(28)],
+                                    help_text='Voltage Range - Maximal Voltage', verbose_name='Maximal Voltage',
+                                    null=True, blank=True)
+
+    antenna_connector = models.ManyToManyField('AntennaConnector')
+    protocol = models.ManyToManyField('ReceiverProtocol',
+                                      verbose_name="Output Protocol", help_text="Rx To FC")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Public if empty",
+                             blank=True, null=True)
+
+    def __str__(self):
+        return self.model
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Receiver'
+        verbose_name_plural = 'Receivers'
+        ordering = ['manufacturer', 'model',]
+
+
 class Camera(models.Model, UniqueConstraintMixin):
     fields_public = ['manufacturer', 'model',
-                     'tvl', 'voltage_min', 'voltage_max', 'ratio', 'fov', 'output_type', 'light_sens',
-                     'height', 'width', 'height_bracket', 'width_bracket',
+                     'tvl', 'voltage_min', 'voltage_max', 'ratio', 'fov',
+                     'output_type', 'light_sens',
                      ]
     fields_private = fields_public + ['user', ]
     error_message = 'A Camera with these attributes already exists for this user.'
@@ -61,16 +94,6 @@ class Camera(models.Model, UniqueConstraintMixin):
     light_sens = models.CharField(max_length=10, choices=SensitivityChoices.choices, verbose_name='Light Sensitivity',
                                   help_text='Higher light sensitivity = Better night vision',
                                   default=SensitivityChoices.UNKNOWN)
-
-    height = models.FloatField(max_length=5, help_text="Height of the camera in mm",
-                               verbose_name="Camera mount size height")
-    width = models.FloatField(max_length=5, help_text="Width of the camera in mm",
-                              verbose_name="Camera mount size width")
-
-    height_bracket = models.FloatField(max_length=5, help_text="Height of the camera in mm with bracket",
-                                       verbose_name="Camera height with bracket", blank=True, null=True)
-    width_bracket = models.FloatField(max_length=5, help_text="Width of the camera in mm with bracket",
-                                      verbose_name="Camera width with bracket", blank=True, null=True)
 
     weight = models.FloatField(help_text='Weight oh the camera in grams',
                                blank=True, null=True)
@@ -124,10 +147,8 @@ class Propeller(models.Model, UniqueConstraintMixin):
 
 
 class Frame(models.Model, UniqueConstraintMixin):
-    fields_public = ['manufacturer', 'model', 'prop_size', 'size', 'weight', 'material', 'configuration',
-                     'camera_mount_size_height', 'camera_mount_size_width',
-                     'motor_mount_size_height', 'motor_mount_size_width',
-                     'vtl_mount_size_height', 'vtl_mount_size_width',
+    fields_public = ['manufacturer', 'model', 'prop_size', 'size',
+                     'weight', 'material', 'configuration'
                      ]
     fields_private = fields_public + ['user', ]
     error_message = 'A Frame with these attributes already exists for this user.'
@@ -148,8 +169,104 @@ class Frame(models.Model, UniqueConstraintMixin):
                                  verbose_name="Propeller size")
     size = models.CharField(max_length=50, help_text="Size(Diagonal) of the frame in mm",
                             verbose_name="Frame size")
+
     weight = models.FloatField(help_text='Weight oh the frame in grams',
                                blank=True, null=True)
+    material = models.CharField(max_length=50, choices=MaterialChoice.choices,
+                                blank=True, null=True)
+    configuration = models.CharField(max_length=50, choices=ConfigurationChoice.choices,
+                                     blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Public if empty",
+                             blank=True, null=True)
+
+    def __str__(self):
+        # self.frame_detail.count()
+        return self.model
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Frame'
+        verbose_name_plural = 'Frames'
+        ordering = ['manufacturer', 'model', ]
+
+
+# class Stack(models.Model):
+#     raise NotImplementedError
+
+# class Antenna(models.Model, UniqueConstraintMixin):
+#     raise NotImplementedError
+
+class ReceiverDetail(models.Model):
+    receiver = models.ForeignKey('Receiver', on_delete=models.PROTECT, related_name='receiver_details')
+
+    frequency = models.FloatField(validators=[MinValueValidator(0), ], )
+    weight = models.FloatField(validators=[MinValueValidator(0), ], )
+    telemetry_power = models.FloatField(validators=[MinValueValidator(0), ], help_text="Telemetry Power, In dBm")
+    rf_chip = models.CharField(max_length=50, help_text="RF Chip Number",
+                               null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.frequency}"
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Receiver Detail'
+        verbose_name_plural = 'Receiver Details'
+        ordering = ['receiver', 'frequency', 'weight', 'telemetry_power', 'rf_chip']
+        unique_together = ['receiver', 'frequency', 'weight', 'telemetry_power', 'rf_chip']
+
+
+class ReceiverProtocol(models.Model):
+    type = models.CharField(max_length=50, verbose_name="Output Protocol Type", help_text="Rx To FC")
+    is_custom = models.BooleanField(default=True, help_text="Created by user?")
+
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        verbose_name = 'Receiver Protocol'
+        verbose_name_plural = 'Receiver Protocols'
+        ordering = ['type', '-is_custom']
+
+
+class AntennaConnector(models.Model):
+    type = models.CharField(max_length=50, help_text="Type of antenna connector")
+    is_custom = models.BooleanField(default=True, help_text="Created by user?")
+
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        verbose_name = 'Antenna Connector'
+        verbose_name_plural = 'Antenna Connectors'
+        ordering = ['type', '-is_custom']
+
+
+class CameraDetail(models.Model):
+    camera = models.ForeignKey('Camera', on_delete=models.CASCADE)
+
+    height = models.FloatField(max_length=5, help_text="Height of the camera in mm",
+                               verbose_name="Camera mount size height")
+    width = models.FloatField(max_length=5, help_text="Width of the camera in mm",
+                              verbose_name="Camera mount size width")
+
+    def __str__(self):
+        return f'{self.height}x{self.width}'
+
+    class Meta:
+        verbose_name = 'Camera Detail'
+        verbose_name_plural = 'Camera Details'
+        unique_together = ['camera', 'height', 'width']
+
+
+class FrameDetail(models.Model):
+    frame = models.ForeignKey('Frame', on_delete=models.CASCADE, related_name='frame_details')
 
     camera_mount_height = models.FloatField(max_length=5, help_text="Height of the camera in mm",
                                             verbose_name="Camera mount size height")
@@ -166,24 +283,19 @@ class Frame(models.Model, UniqueConstraintMixin):
     vtx_mount_width = models.FloatField(max_length=5, help_text="Width of the vtx in mm",
                                         verbose_name="VTX mount size width")
 
-    material = models.CharField(max_length=50, choices=MaterialChoice.choices,
-                                blank=True, null=True)
-    configuration = models.CharField(max_length=50, choices=ConfigurationChoice.choices,
-                                     blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Public if empty",
-                             blank=True, null=True)
-
     def __str__(self):
-        return self.model
+        return self.frame.model
 
-    def save(self, *args, **kwargs):
-        self.full_clean()
-        super().save(*args, **kwargs)
+    def delete(self, *args, **kwargs):
+        if self.frame.frame_details.count() > 1:
+            super().delete(*args, **kwargs)
+        else:
+            raise models.ProtectedError("Cannot delete the only FrameDetail for this Frame.", self)
 
     class Meta:
-        verbose_name = 'Frame'
-        verbose_name_plural = 'Frames'
-        ordering = ['manufacturer', 'model', ]
-
-# class Stack(models.Model):
-#     raise NotImplementedError
+        verbose_name = 'FrameDetail'
+        verbose_name_plural = 'FrameDetails'
+        ordering = ('frame__manufacturer', 'frame__model')
+        unique_together = ['frame', 'camera_mount_height', 'camera_mount_width',
+                           'motor_mount_height', 'motor_mount_width',
+                           'vtx_mount_height', 'vtx_mount_width']
--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jazzmin',
+    'django_extensions',
 
     'django.contrib.admin',
     'django.contrib.auth',
